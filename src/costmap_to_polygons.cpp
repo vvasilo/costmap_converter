c87923a897df82eb35a182a7d5bfdb713de9b15a
Vasileios Vasilopoulos <vasilis.vasilop@gmail.com>
2022-04-18 23:40:34 -0400

    Ignore unknown space in polygon construction

--- src/costmap_to_polygons.cpp
+++ src/costmap_to_polygons.cpp
@@ -213,7 +213,7 @@ void CostmapToPolygonsDBSMCCH::updateCostmap2D()
         for(std::size_t j = 0; j < costmap_->getSizeInCellsY(); j++)
         {
           int value = costmap_->getCost(i,j);
-          if(value >= costmap_2d::LETHAL_OBSTACLE)
+          if(value >= costmap_2d::LETHAL_OBSTACLE && value < costmap_2d::NO_INFORMATION)
           {
             double x, y;
             costmap_->mapToWorld((unsigned int)i, (unsigned int)j, x, y);
